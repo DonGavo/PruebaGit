214b2367da79d3b84bf64c2d15291b6ab91caeb9
DonGavo <cesarsanabria4321@hotmail.com>
2024-06-10 22:23:06 -0500

    Se corrigen los archivos

---
+++ EjercicioGit/EjercicioGit/EjercicioGit/Datos/ClConexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGit.Datos
+{
+    internal class ClConexion
+    {
+        private SqlConnection oConexion = null;
+
+        public ClConexion()
+        {
+            oConexion = new SqlConnection("Data Source=DESKTOP-0K2FE29\\SQLEXPRESS;Initial Catalog=dbIntegradorMayo;Integrated Security=True;");
+        }
+        public SqlConnection MtdAbrirConexion()
+        {
+            oConexion.Open();
+            return oConexion;
+        }
+        public void MtdCerrarConexion()
+        {
+            oConexion.Close();
+        }
+    }
+}
