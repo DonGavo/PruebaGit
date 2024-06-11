5187f6a6656369fd0ad0d550d7b25fefb261dce8
DonGavo <cesarsanabria4321@hotmail.com>
2024-06-10 22:10:41 -0500

    Proyecto Inicial Cargado

---
+++ EjercicioGit/EjercicioGit/Datos/ClSqlAdmin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGit.Datos
+{
+    internal class ClSqlAdmin
+    {
+        public DataTable mtdSelect(string consulta)
+        {
+            ClConexion oConexion = new ClConexion();
+            SqlConnection conexion = oConexion.MtdAbrirConexion();
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, conexion);
+
+            DataTable datos = new DataTable();
+            adapter.Fill(datos);
+            oConexion.MtdCerrarConexion();
+            return datos;
+        }
+
+        public int mtdIUD(string consulta)
+        {
+            ClConexion oConexion = new ClConexion();
+            SqlConnection sqlConnection = oConexion.MtdAbrirConexion();
+            SqlCommand cmd = sqlConnection.CreateCommand();
+            cmd.CommandText = consulta;
+            int resultado = cmd.ExecuteNonQuery();
+            return resultado;
+        }
+
+    }
+}
